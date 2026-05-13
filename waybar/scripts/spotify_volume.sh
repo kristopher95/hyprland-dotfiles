fa9a4f2175335be59d59a93e4e515642fdbb4acd
kristopher95 <kristophervessella@hotmail.com>
2026-05-13 01:02:07 -0400

    Add Spotify volume controls to Waybar

---
+++ waybar/scripts/spotify_volume.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+vol="$(playerctl -p spotify volume 2>/dev/null)"
+
+if [[ -z "$vol" ]]; then
+    echo ""
+    exit 0
+fi
+
+percent="$(awk -v v="$vol" 'BEGIN { printf "%.0f", v * 100 }')"
+
+echo "[S-VOL: ${percent}%]"
